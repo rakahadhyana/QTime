fa6c940e27546e8c8779e91cef838b66ef671db6
Raka Hadhyana <13516099@std.stei.itb.ac.id>
2019-02-25 13:53:51 +0700

    add done queue listener

--- app/src/main/java/com/hmm/q_time/QueueFragment.java
+++ app/src/main/java/com/hmm/q_time/QueueFragment.java
@@ -7,7 +7,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 /**
@@ -29,6 +45,18 @@ public class QueueFragment extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    private FirebaseAuth.AuthStateListener authListener;
+    private FirebaseAuth auth;
+
+    private ImageButton mButtonDone;
+    private ImageButton mButtonCancel;
+    private TextView mNameTextView;
+    private TextView mCurrentQueue;
+
+    private String doctorId;
+    private int currentQueue;
+    private Boolean isInQueue;
 
     public QueueFragment() {
         // Required empty public constructor
@@ -59,13 +87,42 @@ public class QueueFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        //get firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        //get current user
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        isOnQueueRequest();
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_queue, container, false);
+        View v = inflater.inflate(R.layout.fragment_queue, container, false);
+        mButtonDone = v.findViewById(R.id.done);
+        mButtonDone.setEnabled(false);
+        mButtonDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Done Request
+                Toast.makeText(getContext(),"DONE",Toast.LENGTH_SHORT).show();
+            }
+        });
+        mButtonCancel = v.findViewById(R.id.cancel);
+        mButtonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Cancel Request
+                Toast.makeText(getContext(),"CANCEL",Toast.LENGTH_SHORT).show();
+            }
+        });
+        mNameTextView = v.findViewById(R.id.doctor_detail_name);
+        mCurrentQueue = v.findViewById(R.id.doctor_detail_current_queue);
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -106,4 +163,65 @@ public class QueueFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+    private void isOnQueueRequest(){
+        String URL = "https://qtime-android.herokuapp.com/api/queue/user/" + auth.getCurrentUser().getEmail();
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONObject queue = response.getJSONObject("queue");
+                    doctorId = queue.getString("doctor");
+                    mNameTextView.setText(doctorId);
+                    if(queue != null){
+                        isInQueue = true;
+                        getQueueNumber();
+                    }else{
+                        isInQueue = false;
+                    }
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        Volley.newRequestQueue(getContext()).add(request);
+    }
+
+    private void getQueueNumber(){
+        String URL = "https://qtime-android.herokuapp.com/api/queue/doctor/"+ doctorId;
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray queues = response.getJSONArray("queues");
+                    for (int i = 0; i < queues.length(); i++){
+                        JSONObject queue = queues.getJSONObject(i);
+                        String user = queue.getString("user");
+                        if(user.equals(auth.getCurrentUser().getEmail())){
+                            currentQueue = i + 1;
+                            mCurrentQueue.setText("Current queue: "+currentQueue);
+                            mCurrentQueue.setTextColor(getResources().getColor(R.color.black));
+                            if(currentQueue == 0){
+                                mButtonDone.setEnabled(true);
+                            }
+                        }
+                    }
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        Volley.newRequestQueue(getContext()).add(request);
+    }
+
 }
